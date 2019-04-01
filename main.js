2f4145dc60c434550b763de6dba7b145f425e98f
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-04-02 02:28:27 +0530

    page appearance changed

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function checking()
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=af3f72199c0b9f7577f413ba180fecc7",
         dataType: 'json',
         success: function (data) {
-            console.log(data.main.temp);
-            $("#temp").html(data.main.temp);
+            console.log(data);
+            $("#temp").html(data.main.temp+"<sup>o</sup> C");
             $(".icon").attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
         }
     });
