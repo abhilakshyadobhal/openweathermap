5bdbe7db8c2083551aec362e04d0766c8f1a2e49
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-04-01 11:58:24 +0530

    weather api used

---
+++ main.js
@@ -0,0 +1,33 @@
+$('button').on('click',checking);
+// $(document).ready(function () {
+//     $('button').bind('click',function () {
+//         if (city != '') {
+
+//             $.ajax({
+//                 type: 'GET',
+//                 url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=af3f72199c0b9f7577f413ba180fecc7",
+//                 dataType: 'json',
+//                 success: function (data) {
+//                     console.log(data.main.temp);
+//                     $("#temp").html(data.main.temp);
+//                     $(".icon").attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+//                 }
+//             });
+
+//         }
+//     })
+// });
+function checking()
+{
+    var city = $("#city").val();
+    $.ajax({
+        type: 'GET',
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=af3f72199c0b9f7577f413ba180fecc7",
+        dataType: 'json',
+        success: function (data) {
+            console.log(data.main.temp);
+            $("#temp").html(data.main.temp);
+            $(".icon").attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        }
+    });
+}
